24a16dccca4bffddbb0d0152148845750b75d1d1
Alfred <alfred910226@gmail.com>
2023-11-15 21:36:26 +0800

    refactor: cassandra setup 移到 main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from cassandra.cqlengine.management import sync_table
 from cassandra.cqlengine import connection
@@ -5,15 +6,16 @@ from cassandra.cqlengine import connection
 from app.routers import users
 from app.models.users import User, TokenRevoked
 
-app = FastAPI()
-
-app.include_router(users.router)
-
-@app.on_event("startup")
-async def create_table():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     connection.setup(['cassandra'], "article_express", port=9042, protocol_version=3)
     sync_table(User)
     sync_table(TokenRevoked)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(users.router)
 
 @app.get("/")
 async def main():
