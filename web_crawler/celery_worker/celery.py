6a6a0df30c61ebaf0a36f7708448b2f3a01ebc74
Alfred <alfred910226@gmail.com>
2024-01-07 10:34:19 +0800

    feat: 移除 celery 架構的爬蟲

--- web_crawler/celery_worker/celery.py
+++
@@ -1,4 +0,0 @@
-from celery import Celery
-
-celery=Celery('web-crawler')
-celery.config_from_object('celery_worker.config')
--- web_crawler/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-nohup celery -A celery_worker.celery:celery worker -l INFO -Q urls -n urls@%h --logfile=/celery_worker/celery_log/urls@%h.log --pool=threads --concurrency=100 --without-gossip --without-mingle &
-celery -A celery_worker.celery:celery worker -l INFO -Q soup -n soup@%h --logfile=/celery_worker/celery_log/soup@%h.log --pool=threads --concurrency=100 --without-gossip --without-mingle 
